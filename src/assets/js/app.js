3aaeedf94243cb728be5b82fc8e2b11c928d3af9
Christian Mejlak <chris@mejlak.com>
2020-09-28 11:58:07 +0200

    finish testimonials carousel

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -49,3 +49,164 @@ class Dropdown {
         }
     });
 })();
+
+class Carousel {
+    constructor(carousel) {
+        this._carousel = carousel;
+        this._slides = carousel.children[0].children;
+        this._buttons = carousel.children[1].children;
+        this._currentSlide = 0;
+        this._slideWidth = 0;
+        this._carouselHeight = 0;
+        this._lastSlide = this._slides.length - 1;
+        this._isDown = false;
+        this._initialXPosition = 0;
+        this._currentSlidesWrapperTransform = 0;
+        this._resistance = 100; // the amount of pixel drag to witch slide
+
+        this.calculateSlideWidth();
+        this.addCurrentActive();
+        this.addListeners();
+    }
+
+    addListeners = () => {
+        // ensure slide widy
+        window.addEventListener("resize", () => {
+            this.calculateSlideWidth();
+            this.goToSlide(this._currentSlide);
+        });
+        for (let item of this._buttons) {
+            item.addEventListener("click", this.decideSlideToShow);
+        }
+
+        // Listeners for dragging
+        this._carousel.addEventListener("mousedown", this.handleDragStart);
+        this._carousel.addEventListener("touchstart", this.handleDragStart, {
+            passive: true,
+        });
+        this._carousel.addEventListener("mousemove", this.handleDragMove);
+        this._carousel.addEventListener("touchmove", this.handleDragMove, {
+            passive: true,
+        });
+        this._carousel.addEventListener("mouseup", this.handleDragStop);
+        this._carousel.addEventListener("touchend", this.handleDragStop);
+    };
+
+    calculateSlideWidth = () => {
+        const rect = this._carousel.getBoundingClientRect();
+        this._slideWidth = rect.right - rect.left;
+    };
+
+    // When draggin we need to find the current drag position
+    getCurrentSlidesWrapperTransform = () => {
+        let currentTransformX = getComputedStyle(this._carousel.children[0])
+            .getPropertyValue("transform")
+            .match(/(-?[0-9\.]+)/g)[4];
+        return parseInt(currentTransformX);
+    };
+
+    //
+    slideTrack = () => {
+        this._carousel.children[0].style.transform = `translateX(-${
+            this._currentSlide * this._slideWidth
+        }px)`;
+    };
+
+    decideSlideToShow = (event) => {
+        const target = event.currentTarget.dataset.slide;
+        if (target === "next") {
+            this.nextSlide();
+        } else if (target === "prev") {
+            this.prevSlide();
+        } else {
+            this.goToSlide(parseInt(target));
+        }
+    };
+
+    goToNextSlide = () => {
+        this.removeCurrentActive();
+        if (this._currentSlide >= this._lastSlide) {
+            this._currentSlide = 0;
+        } else {
+            this._currentSlide += 1;
+        }
+        this.addCurrentActive();
+    };
+
+    goToPrevSlide = () => {
+        this.removeCurrentActive();
+        if (this._currentSlide <= 0) {
+            this._currentSlide = this._lastSlide;
+        } else {
+            this._currentSlide -= 1;
+        }
+        this.addCurrentActive();
+    };
+
+    goToSlide = (target) => {
+        this.removeCurrentActive();
+        this._currentSlide = target;
+        this.addCurrentActive();
+    };
+
+    removeCurrentActive = () => {
+        for (let button of this._buttons) {
+            if (button.dataset.slide == this._currentSlide) {
+                button.classList.remove("active");
+            }
+        }
+        this._slides[this._currentSlide].classList.remove("active");
+    };
+
+    addCurrentActive = () => {
+        for (let button of this._buttons) {
+            if (button.dataset.slide == this._currentSlide) {
+                button.classList.add("active");
+            }
+        }
+        this._slides[this._currentSlide].classList.add("active");
+        this.slideTrack();
+    };
+
+    handleDragStart = (e) => {
+        this._carousel.classList.add("dragging");
+        this._isDown = true;
+        this._initialXPosition = e.pageX || e.touches[0].clientX;
+        this._currentSlidesWrapperTransform = this.getCurrentSlidesWrapperTransform();
+    };
+
+    handleDragMove = (e) => {
+        if (this._isDown) {
+            const posX = e.pageX || e.touches[0].clientX;
+            const movement = posX - this._initialXPosition;
+            this._carousel.children[0].style.transform = `translateX(${
+                movement + this._currentSlidesWrapperTransform
+            }px)`;
+        }
+    };
+
+    handleDragStop = () => {
+        this._isDown = false;
+        this._carousel.classList.remove("dragging");
+        const startingTransform = this._currentSlide * this._slideWidth * -1; // this returns positive when it is infact a negative hence *-1
+        const currentTransform = this.getCurrentSlidesWrapperTransform();
+
+        if (currentTransform + this._resistance < startingTransform) {
+            this.goToNextSlide();
+        } else if (currentTransform - this._resistance > startingTransform) {
+            this.goToPrevSlide();
+        } else {
+            this.goToSlide(this._currentSlide);
+        }
+    };
+}
+
+(function () {
+    window.addEventListener("load", () => {
+        const carousels = document.querySelectorAll(".zen-carousel");
+        for (let carousel of carousels) {
+            new Carousel(carousel);
+            console.log(carousel.children[0].children);
+        }
+    });
+})();
