45bfca592b9e164be7c28fae3ffaf846e18ddd99
= <=>
2020-10-12 17:15:38 +0200

    final revisions

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 module.exports = async function() {
-    const postsURL = 'http://localhost/semeia/wp-json/mejlak/v1/posts';
-    // const propertiesURL = 'https://look-gozo.reachadvertising.biz/wp-json/mejlak/v1/properties';
+    // const postsURL = 'http://localhost/semeia/wp-json/mejlak/v1/posts';
+    const postsURL = 'https://semeia.reachadvertising.biz/wp-json/mejlak/v1/posts';
 
     const response = await axios.get(postsURL);
     return response.data;
--- src/_data/vacancies.js
+++ src/_data/vacancies.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
 module.exports = async function() {
-	const vacanciesURL = "http://localhost/semeia/wp-json/mejlak/v1/vacancies";
-
+	// const vacanciesURL = "http://localhost/semeia/wp-json/mejlak/v1/vacancies";
+	const vacanciesURL = "https://semeia.reachadvertising.biz/wp-json/mejlak/v1/vacancies";
 	const response = await axios.get(vacanciesURL);
 	return response.data;
 }
