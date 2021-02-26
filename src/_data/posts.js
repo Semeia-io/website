22c57e784bc99f166029db44655d53a2103f5871
Christian Mejlak <chris@mejlak.com>
2021-02-26 14:12:13 +0100

    feat: add WP functionality

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = async function() {
-    const postsURL = 'https://semeia.reachadvertising.biz/wp-json/mejlak/v1/posts';
+    const postsURL = 'http://cms.semeia.io/wp-json/mejlak/v1/posts';
 
     const response = await axios.get(postsURL);
     return response.data;
--- src/_data/vacancies.js
+++ src/_data/vacancies.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
-// module.exports = async function() {
-// 	const vacanciesURL = "https://semeia.reachadvertising.biz/wp-json/mejlak/v1/vacancies";
-// 	const response = await axios.get(vacanciesURL);
-// 	return response.data;
-// }
+module.exports = async function() {
+	const vacanciesURL = "http://cms.semeia.io/wp-json/mejlak/v1/vacancies";
+	const response = await axios.get(vacanciesURL);
+	return response.data;
+}
