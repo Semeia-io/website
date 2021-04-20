4b1dfd3e6f235802f3dc3ab78a3f4835d915ad41
Christian Mejlak <chris@mejlak.com>
2021-04-20 07:31:26 +0200

    fix: wordpress data to https

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = async function() {
-    const postsURL = 'http://cms.semeia.io/wp-json/mejlak/v1/posts';
+    const postsURL = 'https://cms.semeia.io/wp-json/mejlak/v1/posts';
 
     const response = await axios.get(postsURL);
     return response.data;
--- src/_data/vacancies.js
+++ src/_data/vacancies.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 module.exports = async function() {
-	const vacanciesURL = "http://cms.semeia.io/wp-json/mejlak/v1/vacancies";
+	const vacanciesURL = "https://cms.semeia.io/wp-json/mejlak/v1/vacancies";
 	const response = await axios.get(vacanciesURL);
 	return response.data;
 }
